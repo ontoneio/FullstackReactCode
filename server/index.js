32868895fae237e1c2b49328cf8b259f5b65c96d
ontoneio <ontoneio.mm@gmail.com>
2018-03-09 16:14:09 -0800

    Update express server to handle resources based on environment

--- server/index.js
+++ server/index.js
@@ -28,6 +28,15 @@ app.use(passport.session());
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve production assets - main.js or main.css
+  app.use(express.static('client/build'))
+  // Express will serve index.html if Express does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))    
+  });
+}
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server listening on ${PORT}`));
