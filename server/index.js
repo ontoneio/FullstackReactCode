dc5c1a29025c2463a0b5a26d499d2909d01604a0
ontoneio <ontoneio.mm@gmail.com>
2018-03-12 21:43:20 -0700

    Make server default route '*' relative again

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve production assets - main.js or main.css
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Express will serve index.html if Express does not recognize the route
-  app.get('/*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))    
   });
 }
